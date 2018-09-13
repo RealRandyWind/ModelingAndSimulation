cd5fdcc532cde1589bbb027a96da3e806a4502c4
Randy Wind <randywind@live.nl>
2018-09-13 13:22:52 +0200

    Use NDev Template Library

---
+++ Configurations.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#ifdef MODELINGANDSIMULATION_EXPORTS
+#define ModelingAndSimulation_Exports __declspec(dllexport)
+#else
+#define ModelingAndSimulation_Exports __declspec(dllimport)
+#endif
